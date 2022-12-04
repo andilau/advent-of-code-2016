16c738c3e6f82198422dbbf94d06859aaedef1a6
Andreas Lau <andilau@users.noreply.github.com>
2022-12-04 15:26:11 +0100

    Day 1: No Time for a Taxicab (#6)
    * Day 1
    
    * Day 1 WIP
    
    * Day 1
    
    * Day 1
    
    * Day 1: No Time for a Taxicab
    
    * Day 1: No Time for a Taxicab

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -1,12 +1,75 @@
 package days
 
-@AdventOfCodePuzzle(
-    name = "Puzzle Name",
-    url = "https://adventofcode.com/2000/day/1",
-    date = Date(day = 1, year = 2000)
-)
-class Day1(private val input: List<Int>) : Puzzle {
-    override fun partOne() = input.sum()
+import days.Day1.Direction.*
+import kotlin.math.abs
 
-    override fun partTwo() = input.foldRight(1) { element, acc -> element * acc }
+@AdventOfCodePuzzle(
+    name = "No Time for a Taxicab",
+    url = "https://adventofcode.com/2016/day/1",
+    date = Date(day = 1, year = 2016)
+)
+class Day1(input: String) : Puzzle {
+
+    private val instructions = input
+        .split(", ")
+        //.filter { s -> s.length >= 2 }
+        .map(Instruction::from)
+
+    override fun partOne(): Int = instructions.walk().last().manhattanDistance
+
+    override fun partTwo(): Int {
+        val visited = mutableSetOf<Point>()
+
+        for (point in instructions.walk()) {
+            if (point in visited)
+                return point.manhattanDistance
+            visited += point
+        }
+        error("Invalid Input")
+    }
+
+    private fun List<Instruction>.walk() = sequence<Point> {
+        var pos = Point.ORIGIN
+        var dir = NORTH
+        this@walk.forEach() { instruction ->
+            dir = dir.turn(instruction.turn)
+            (1..instruction.steps).forEach { _ ->
+                pos = pos.move(dir)
+                yield(pos)
+            }
+        }
+    }
+
+    data class Point(val x: Int, val y: Int) {
+        fun move(facing: Direction): Point = when (facing) {
+            NORTH -> copy(y = y + 1)
+            SOUTH -> copy(y = y - 1)
+            EAST -> copy(x = x + 1)
+            WEST -> copy(x = x - 1)
+        }
+
+        val manhattanDistance get() = abs(x) + abs(y)
+
+        companion object {
+            val ORIGIN: Point = Point(0, 0)
+        }
+    }
+
+    data class Instruction(val turn: Char, val steps: Int) {
+        companion object {
+            fun from(text: String): Instruction =
+                Instruction(text.first(), text.substring(1).toInt())
+        }
+    }
+
+    enum class Direction {
+        NORTH, EAST, SOUTH, WEST;
+
+        fun turn(turn: Char): Direction = when (this) {
+            NORTH -> if (turn == 'R') EAST else WEST
+            EAST -> if (turn == 'R') SOUTH else NORTH
+            SOUTH -> if (turn == 'R') WEST else EAST
+            WEST -> if (turn == 'R') NORTH else SOUTH
+        }
+    }
 }
--- src/test/kotlin/days/Day1Test.kt
+++ src/test/kotlin/days/Day1Test.kt
@@ -1,39 +1,35 @@
 package days
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 
 @DisplayName("Day 1")
 class Day1Test {
-    val ints = listOf(1, 2, 3)
 
     @Nested
     @DisplayName("Part 1")
     inner class Part1 {
-        @Test
-        fun shouldReturnSumOfEmptyList() {
-            assertThat(Day1(emptyList()).partOne()).isEqualTo(0)
-        }
-
-        @Test
-        fun shouldReturnSumOfSimpleList() {
-            assertThat(Day1(ints).partOne()).isEqualTo(6)
+        @TestFactory
+        fun `How far is the shortest path to the destination`() = listOf(
+            "R2, L3" to 5,
+            "R2, R2, R2" to 2,
+            "R5, L5, R5, R3" to 12,
+        ).map { (instructions, distance) ->
+            DynamicTest.dynamicTest("""Example $instructions shortest path ist '$distance' away" """) {
+                assertThat(Day1(instructions).partOne()).isEqualTo(distance)
+            }
         }
     }
 
+    // R8, R4, R4, R8
     @Nested
     @DisplayName("Part 2")
     inner class Part2 {
-        @Test
-        fun shouldReturnProductOfEmptyList() {
-            assertThat(Day1(emptyList()).partTwo()).isEqualTo(1)
-        }
-
-        @Test
-        fun shouldReturnProductOfSimpleList() {
-            assertThat(Day1(ints).partTwo()).isEqualTo(6)
+        @TestFactory
+        fun `How many blocks away is the first location you visit twice`() = listOf("R8, R4, R4, R8" to 4).map { (instructions, distance) ->
+            DynamicTest.dynamicTest("""Example $instructions first location you visit twice is '$distance' away""") {
+                assertThat(Day1(instructions).partTwo()).isEqualTo(distance)
+            }
         }
     }
 }
