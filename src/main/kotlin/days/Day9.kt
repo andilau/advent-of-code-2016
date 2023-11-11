435660156b6ce3728511a64228eb177d335151d2
Andreas Lau <andreas.lau@europace.de>
2023-11-11 15:50:55 +0100

    Day 9: Explosives in Cyberspace, Start

---
+++ src/main/kotlin/days/Day9.kt
@@ -0,0 +1,15 @@
+package days
+
+@AdventOfCodePuzzle(
+    name = "Explosives in Cyberspace",
+    url = "https://adventofcode.com/2016/day/9",
+    date = Date(day = 9, year = 2016)
+)
+class Day9(private val input: List<String>) : Puzzle {
+
+    override fun partOne(): Int = 0
+
+    override fun partTwo() = "CFLELOYFCS"
+
+
+}
